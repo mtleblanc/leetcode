fa5c6ead8c6f776beaa2c156267df1b613071d45
Michael LeBlanc <leblanc.michael@gmail.com>
2024-07-08 01:29:14 -0400

    517. Full proof of correctness

--- rust/src/p517_super_washing_machines.rs
+++ rust/src/p517_super_washing_machines.rs
@@ -1,24 +1,74 @@
 impl Solution {
     /*
-     * For each index we calculate how many dresses have
-     * to move left of it and how many dresses have to move
-     * right.  The machine only does work if these numbers
-     * are positive (negatives means dresses are provided to it)
+     * Each machine must pass some number of dresses
+     * left (l[k] below) and some number right (r[k]),
+     * which can be calculated in a single pass comparing
+     * prefix sums of the actual and average values.
      *
-     * I claim that the largest amount of work for a single machine
-     * answers the question with a sketch of a proof as follows.
+     * I claim that the machines can be balanced in the max
+     * of the sums of these values, and can be done simply
+     * by greedily moving dresses whenever possible.
      *
-     * The idea is that if on a move a machine needs to pass a dress
-     * but is empty, then there must be a machine next to it that
-     * has more work.  Suppose wolog that the machine needs to pass
-     * to the right.  Since there are 0 dresses currently in the
-     * machine, it cannot need to pass both left and rigth, so its
-     * work is only what must be passed right.
-     * Then this means there is above average number
-     * of total dresses to the left of it (inclusive).  But it has
-     * 0 dresses, so the machine immediately to its left has
-     * work at least one average higher than this machine.
+     * Idea: calculate how many times a machine must move
+     * dresses both left and right, and show that making
+     * such a move a) decreases the calculated values, and
+     * b) is always possible for the machine with the most
+     * dresses to move.
+     *
+     * Proof:
+     * Define s[k] to be the prefix sum of net movement
+     * required (ie sum (avg - machine[k])).  Then
+     * define l[k] as the positive values in s[k], offset
+     * by 1 (that is l[k] = 0 if s[k-1] < 0, s[k-1] otherwise)
+     * and r[k] as the opposite of the negative values with
+     * no offset (r[k] = -s[k] if s[k] < 0)
+     *
+     * Now suppose l[k] > 0.  Then if we move a dress
+     * from machine k to machine k-1, then s[k] remains
+     * unchanged (as the dress is still in the prefix)
+     * and s[k-1] decreases by 1, and s[k-2] and beyond
+     * remain the same (as the dress remains out of the
+     * prefix).  Since l[k] = s[k-1] > 0 before the move,
+     * we still have s[k-1] >= 0 after the move.  Thus
+     * r[k-1] = 0 both before and after.  Therefore
+     * moving a dress left from a machine k with l[k] > 0
+     * decreases l[k] and leaves the remaining l and r values
+     * unchanged.
+     *
+     * Likewise, moving a dress right from machine k
+     * with r[k] > 0 increases s[k] by 1 and leaves
+     * the others unchanged.  s[k] = r[k] < 0 beforehand
+     * and so s[k] <= 0 after, and so likewise only r[k]
+     * decreases by 1 and other l, r remain unchanged.
+     *
+     * Our moves will be greedy, moving a dress left
+     * from machine k if l[k] > 0 and it has dresses,
+     * otherwise to the right if r[k] > 0 and it has
+     * dresses, and doing nothing if it has no dresses
+     * or if both l[k] and r[k] are 0.
+     *
+     * By the observations above, any machine that moves
+     * a dress in this manner will decrease one of l[k]
+     * or r[k], and no l[k] or r[k] will ever increase.
+     *
+     * We finish by induction, assume we can balance the
+     * machine in m moves if the maximum of l[k] + r[k] is m,
+     * (which is trivially true if m = 0).  Now if the
+     * maximum is m+1, perform the greedy move.  As long
+     * as each machine achieving that maximum has a dress
+     * then it's total will decrease to m.
+     *
+     * Suppose machine k has l[k] + r[k] = m + 1, but has
+     * 0 dresses. Then s[k] = a + s[k-1].  If s[k] >= 0,
+     * we have l[k+1] = s[k], r[k] = 0.  Then
+     * s[k-1] = l[k] = m + 1, and so l[k] = a + m + 1,
+     * contradicting the maximal value being m + 1.
+     *
+     * If s[k] < 0, s[k-1] = s[k] - a < 0, and so l[k] = 0
+     * and r[k] = m + 1.  Then r[k-1] = m + 1 + a, again
+     * a contradiction and completing the proof.
      */
+
     pub fn find_min_moves(machines: Vec<i32>) -> i32 {
         let count = machines.len() as i32;
         let total: i32 = machines.iter().sum();
@@ -49,8 +99,20 @@ mod tests {
     fn test_cases() {
         assert_eq!(Solution::find_min_moves(vec![1, 0, 5]), 3);
         assert_eq!(Solution::find_min_moves(vec![0, 3, 0]), 2);
+        // [-1,2,-1]
+        // [-1,1,0]
+        // [0,1,0]
+        // [0,1,0]
         assert_eq!(Solution::find_min_moves(vec![0, 2, 0]), -1);
         assert_eq!(Solution::find_min_moves(vec![0, 0, 11, 5]), 8);
+        // [-4,-4,7,1]
+        // [-4,-8,-1,0]
+        // [0,4,8,1]
+        // [0,0,0,0]
         assert_eq!(Solution::find_min_moves(vec![9, 1, 8, 8, 9]), 4);
+        //  [2,-6,1,1,2]
+        //  [2,-4,-3,-2,0]
+        //  [2,0,0,0,0]
+        //  [0,0,4,3,2]
     }
 }
